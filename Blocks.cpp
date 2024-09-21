abeb7e583f77c5fb07d833fd210ee88053d180ea
Hartsaxena <64151017+Hartsaxena@users.noreply.github.com>
2024-09-21 14:35:39 -0500

    Dev-Update Added implementation of rotation states and pieces.

--- Blocks.cpp
+++ Blocks.cpp
@@ -1,6 +1,21 @@
 #include "Blocks.hpp"
 
 
+/* Get the value of a specific cell(row, col) in the 4x4 grid */
+bool RotationalStates::getCell(RotationalState state, int row, int col) {
+	int index = 15 - (row * 4 + col); // Calculate inverted bit index (0-15)
+	return (state >> index) & 1; // Shift right and mask the bit
+}
+
+/* Set a specific cell in the 4x4 grid(1 = filled, 0 = empty) */
+void RotationalStates::setCell(RotationalState* state, int row, int col, bool value) {
+	int index = 15 - (row * 4 + col);
+	if (value)
+		*state |= (1 << index); // Set the bit to 1
+	else
+		*state &= ~(1 << index); // Set the bit to 0
+}
+
 void RotationalStateList::push(RotationalState rotationalState) {
 	RotationalStateNode* newNode = new RotationalStateNode(&rotationalState);
 	if (this->head == nullptr) {
@@ -14,32 +29,4 @@ void RotationalStateList::push(RotationalState rotationalState) {
 	newNode->next = this->head;
 	this->tail->next = newNode;
 	this->head = newNode;
-}
-
-IBlock::IBlock() {
-
-}
-
-OBlock::OBlock() {
-
-}
-
-SBlock::SBlock() {
-
-}
-
-ZBlock::ZBlock() {
-
-}
-
-LBlock::LBlock() {
-
-}
-
-TBlock::TBlock() {
-
-}
-
-JBlock::JBlock() {
-
 }
--- Blocks.hpp
+++ Blocks.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 enum BlockType {
 	I_BLOCK,
 	O_BLOCK,
@@ -10,11 +12,61 @@ enum BlockType {
 	J_BLOCK,
 };
 
-class RotationalState {
-public:
 
-private:
-};
+/* 
+*** A 16-bit integer that represents a 4x4 2D matrix. ***
+* Example: the numbers are the indexes of the integer:
+* 0 0 0 0
+* 1 1 1 0
+* 0 1 0 0
+* 0 0 0 0
+* 
+* This matrix represents the rotation of a T-block (Specifically t-block 3)
+* Convert this matrix to binary (from the top left to the bottom right, like you're reading english):
+* 0000 1110 0100 0000
+* Convert binary to decimal: 3648
+*/
+typedef uint16_t RotationalState;
+namespace RotationalStates {
+	bool getCell(RotationalState state, int row, int col);
+	void setCell(RotationalState* state, int row, int col, bool value);
+
+	// Data for rotational states of all pieces.
+	const RotationalState IBlock1 = 3840;
+	const RotationalState IBlock2 = 8738;
+	const RotationalState IBlock3 = 240;
+	const RotationalState IBlock4 = 17476;
+
+	const RotationalState OBlock1 = 52224; // The O-Block only has one orientation, but I'll make 4 variables just in case
+	const RotationalState OBlock2 = 52224;
+	const RotationalState OBlock3 = 52224;
+	const RotationalState OBlock4 = 52224;
+
+	const RotationalState SBlock1 = 27648;
+	const RotationalState SBlock2 = 17952;
+	const RotationalState SBlock3 = 1728;
+	const RotationalState SBlock4 = 35904;
+
+	const RotationalState ZBlock1 = 50688;
+	const RotationalState ZBlock2 = 9792;
+	const RotationalState ZBlock3 = 3168;
+	const RotationalState ZBlock4 = 19584;
+
+	const RotationalState LBlock1 = 11776;
+	const RotationalState LBlock2 = 17504;
+	const RotationalState LBlock3 = 3712;
+	const RotationalState LBlock4 = 50240;
+
+	const RotationalState TBlock1 = 19968;
+	const RotationalState TBlock2 = 17984;
+	const RotationalState TBlock3 = 3648;
+	const RotationalState TBlock4 = 19520;
+
+	const RotationalState JBlock1 = 36352;
+	const RotationalState JBlock2 = 25664;
+	const RotationalState JBlock3 = 3616;
+	const RotationalState JBlock4 = 17600;
+}
 
 typedef struct RotationalStateNode { // Circular singly-linked list
 	explicit RotationalStateNode(RotationalState* data) { this->data = *data; }
@@ -39,7 +91,7 @@ public:
 private:
 	RotationalStateNode* head = nullptr;
 	RotationalStateNode* tail = nullptr;
-	RotationalStateNode* curr = nullptr;
+	RotationalStateNode* curr = nullptr; // For tracking currently active state
 };
 
 class Block {
@@ -51,57 +103,69 @@ protected:
 };
 
 class IBlock : public Block {
-public:
-	IBlock();
-
 private:
-
+	RotationalStateList rotationalStates = RotationalStateList(
+		RotationalStates::IBlock1,
+		RotationalStates::IBlock2,
+		RotationalStates::IBlock3,
+		RotationalStates::IBlock4
+	);
 };
 
 class OBlock : public Block {
-public:
-	OBlock();
-
 private:
-
+	RotationalStateList rotationalStates = RotationalStateList(
+		RotationalStates::OBlock1
+	);
 };
 
 class SBlock : public Block {
-public:
-	SBlock();
-
 private:
+	RotationalStateList rotationalStates = RotationalStateList(
+		RotationalStates::SBlock1,
+		RotationalStates::SBlock2,
+		RotationalStates::SBlock3,
+		RotationalStates::SBlock4
+	);
 
 };
 
 class ZBlock : public Block {
-public:
-	ZBlock();
-
 private:
-
+	RotationalStateList rotationalStates = RotationalStateList(
+		RotationalStates::ZBlock1,
+		RotationalStates::ZBlock2,
+		RotationalStates::ZBlock3,
+		RotationalStates::ZBlock4
+	);
 };
 
 class LBlock : public Block {
-public:
-	LBlock();
-
 private:
-
+	RotationalStateList rotationalStates = RotationalStateList(
+		RotationalStates::LBlock1,
+		RotationalStates::LBlock2,
+		RotationalStates::LBlock3,
+		RotationalStates::LBlock4
+	);
 };
 
 class TBlock : public Block {
-public:
-	TBlock();
-
 private:
-
+	RotationalStateList rotationalStates = RotationalStateList(
+		RotationalStates::TBlock1,
+		RotationalStates::TBlock2,
+		RotationalStates::TBlock3,
+		RotationalStates::TBlock4
+	);
 };
 
 class JBlock : public Block {
-public:
-	JBlock();
-
 private:
-
+	RotationalStateList rotationalStates = RotationalStateList(
+		RotationalStates::JBlock1,
+		RotationalStates::JBlock2,
+		RotationalStates::JBlock3,
+		RotationalStates::JBlock4
+	);
 };
